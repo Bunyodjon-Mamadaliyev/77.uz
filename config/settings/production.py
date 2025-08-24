b4cb9b36188f0f02658f039a826eb4734f90aaee
Bunyodjon Mamadaliyev <bunyodjon.mamadaliyev.2004@gmail.com>
2025-08-25 01:06:27 +0500

    Loyiha serverga tayyor

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,21 +1,36 @@
 from .base import *  # noqa
+from .base import REST_FRAMEWORK as BASE_REST_FRAMEWORK
 
 ALLOWED_HOSTS = [
-    "api.example.com",
-    "admin.example.com" "localhost",
+    "bonx.uz",
+    "www.bonx.uz",
+    "localhost",
     "127.0.0.1",
-    "server_ip_address",
 ]
-CSRF_TRUSTED_ORIGINS = ["https://api.example.com", "https://admin.example.com"]
-CORS_ALLOWED_ORIGINS = ["https://api.example.com", "https://admin.example.com"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://bonx.uz",
+    "https://www.bonx.uz",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://bonx.uz",
+    "https://www.bonx.uz",
+]
+
 DEBUG = False
+
 CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 60 * 60 * 24 * 7 * 52  # one year
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 60 * 60 * 24 * 7 * 52
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = False
 CORS_ORIGIN_ALLOW_ALL = False
 
-REST_FRAMEWORK.update({"DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)})  # noqa: F405
+REST_FRAMEWORK = {
+    **BASE_REST_FRAMEWORK,  # base.py dagi sozlamalar meros bo‘ladi
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+}
